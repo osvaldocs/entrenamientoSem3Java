6424fd7316cd0be1de6de31f73d2eba0dfbb82e5
osvaldocs <100969798+osvaldocs@users.noreply.github.com>
2025-09-26 08:10:08 -0500

    Add files via upload

---
+++ entrenamientoSem3Java/src/controller/MenuController.java
@@ -0,0 +1,51 @@
+package controller;
+
+import javax.swing.*;
+
+public class MenuController {
+
+
+    public void showMenu() {
+        String[] options = {"Add product", "List products", "Update price", "Update stock", "Delete product", "Search product by name", "Exit  with summary"};
+        int option;
+
+            do {
+            option = JOptionPane.showOptionDialog(
+                    null,
+                    "Select an option:",
+                    "User system",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE,
+                    null,
+                    options,
+                    options[0]
+            );
+
+
+            switch (option) {
+                case 0:
+                    addProduct();
+                    break;
+                case 1:
+                    listProducts();
+                    break;
+                case 2:
+                    updateProductPrice();
+                    break;
+                case 3:
+                    updateProductStock();
+                    break;
+                case 4:
+                    deleteProductByID();
+                    break;
+                case 5:
+                    searchProductByName();
+                    break;
+                case 6:
+                    printSummary();
+                    break;
+            }
+
+        } while (option != 6 && option != JOptionPane.CLOSED_OPTION);
+    }
+}
